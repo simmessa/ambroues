adb678329f3de7067504cdbe51c21bf7c15ac2f6
Simmessa <simmessa@gmail.com>
2020-04-21 18:49:10 +0200

    Need to fix sync

--- ambroues.py
+++ ambroues.py
@@ -1,34 +1,68 @@
 import asyncio
 import json
 import time
+from datetime import datetime
 from xknx import XKNX
 from xknx.devices import Light
 
 
 async def init():
+    """Init KNX"""
+    xknx = XKNX(config='xknx.yaml')
+    await xknx.start()
+
     """Read settings"""
     with open('ambroues.json') as json_settings:
         data = json.load(json_settings)
-        print("Ambroues started with settings:")
-        print(json.dumps(data, indent=4))
+        # print("Ambroues started with settings:")
+        # print(json.dumps(data, indent=4))
 
-async def watch():
+        for zone in data['zones']:
+            print("%s starts at %s" % (zone['zone_name'],zone['zone_start_time']) )
+            # irrigation_zone = Light(xknx, name=,
+            #           group_address_switch='2/0/20')
+        return data['zones']
+
+    # await light.set_on()
+    # await asyncio.sleep(2)
+    # await light.set_off()
+    # await xknx.stop()
+
+async def watch(zones):
     while True:
-        seconds = time.time()
-        print("WATERING!(%d)" % seconds)
+        # now = datetime.now()
+        now = datetime.fromisoformat('2020-04-21T17:38:00')
+        now = str(now.hour)+":"+str(now.minute)
+        print("Watching irrigation jobs: (%s)" % now)
+
+        for zone in zones:
+            # starting watering
+            if zone['zone_start_time'] == now:
+                print("%s matches! start watering for %s minutes" % (zone['zone_name'],zone['zone_duration_minutes']) )
+                await water(zone)
+            else:
+                print ("%s doesn't match %s" %(zone['zone_start_time'], now))
+            # stopping watering
         await asyncio.sleep(1)
+
+async def water(zone):
+    print("watering for %s (%s)" % (zone['zone_duration_minutes'], time.time()) )
+    await asyncio.sleep(int(zone['zone_duration_minutes'])*10)
+
 
 def stop():
     watch_task.cancel()
 
 loop = asyncio.get_event_loop()
-#run only X times
-loop.call_later(2, stop)
+#stop after X seconds
+# loop.call_later(3, stop)
 init_task = loop.create_task(init())
-watch_task = loop.create_task(watch())
 
 try:
-    loop.run_until_complete(init_task)
+    zones = loop.run_until_complete(init_task)
+    print(json.dumps(zones, indent=4))
+
+    watch_task = loop.create_task(watch(zones))
     loop.run_until_complete(watch_task)
 except asyncio.CancelledError:
     pass
