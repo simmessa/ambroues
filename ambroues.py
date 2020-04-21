6eb1b6e879b026908b308b2a8ce08b8759c534ad
Simmessa <simmessa@gmail.com>
2020-04-21 15:13:57 +0200

    here's a test with schedule

--- ambroues.py
+++ ambroues.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import schedule
+import time
 from xknx import XKNX
 from xknx.devices import Light
 
@@ -11,6 +12,21 @@ async def main():
         data = json.load(json_settings)
         print("Ambroues started with settings:")
         print(json.dumps(data, indent=4))
+
+    schedule.every().second.do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+loop = asyncio.get_event_loop()
+
+
+# pylint: disable=invalid-name
+
+def job():
+    seconds = time.time()
+    print("WATERING!(%d)" % seconds)
 
     """Connect to KNX/IP bus, switch on light, wait 2 seconds and switch it off again."""
 
@@ -28,10 +44,5 @@ async def main():
     # await asyncio.sleep(2)
     # await light.set_off()
     # await xknx.stop()
-
-
-# pylint: disable=invalid-name
-
-loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 loop.close()
