c79caae13eb4b4f25e4d2dd2e5aaaf4e5c1e6e9a
Simmessa <simmessa@gmail.com>
2020-04-23 09:06:57 +0200

    Asyncio test moving in the right direction

---
+++ asyncio_test.py
@@ -0,0 +1,24 @@
+import asyncio
+
+async def periodic(test, loop):
+    while True:
+        print('periodic')
+        await asyncio.sleep(1)
+        if test == True:
+            loop.call_soon(boom)
+            test = False
+
+def boom():
+    print("BOOM!")
+
+def stop():
+    task.cancel()
+
+test = True
+loop = asyncio.get_event_loop()
+task = loop.create_task(periodic(test, loop))
+
+try:
+    loop.run_until_complete(task)
+except asyncio.CancelledError:
+    pass
